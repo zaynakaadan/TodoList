676c7cf7f848abdf074fe599562128e214b2f1f9
ZAYNA <zk10-91@hotmail.com>
2023-10-02 15:38:37 +0200

    adding some tests

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\App\Entity;
 
+use App\Entity\Task;
+use App\Entity\User;
 use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use App\Entity\User;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Common\Collections\Collection; 
 
 class UserTest extends WebTestCase
 {
@@ -37,10 +39,72 @@ class UserTest extends WebTestCase
         $this->assertEquals("password", $user->getPassword());
         $this->assertEquals($date, $user->getCreatedAt());
         $this->assertEquals($user->getUsername(), $user->getUserIdentifier());
-
-
-
-
-
+        
     }    
+
+    public function testEraseCredentials()
+    {
+        // Create an instance of the User class
+        $user = new User();
+
+        // Call the eraseCredentials method
+        $user->eraseCredentials();
+
+        // Assert that any sensitive data or state in the User class is cleared/reset
+        // For example, if the User class stores a temporary password, ensure it's null after erasing credentials
+        $this->assertNull($user->getPassword());
+    }
+
+    public function testGetTaskRelation()
+    {
+    // Create a User instance
+    $user = new User();
+
+    // Use some logic to add a Task to the User's TaskRelation 
+    $task = new Task();
+    $user->addTaskRelation($task);
+
+    // Call the getTaskRelation method and assert that it returns the added Task
+    $taskRelation = $user->getTaskRelation();
+    $this->assertInstanceOf(Collection::class, $taskRelation);
+    $this->assertCount(1, $taskRelation);
+    $this->assertTrue($taskRelation->contains($task));
+    }
+
+    public function testAddTaskRelation()
+    {
+    // Create a User instance
+    $user = new User();
+
+    // Create a Task instance
+    $task = new Task();
+
+    // Call the addTaskRelation method
+    $user->addTaskRelation($task);
+
+    // Assert that the Task has been added to the User's TaskRelation
+    $taskRelation = $user->getTaskRelation();
+    $this->assertTrue($taskRelation->contains($task));
+    
+    }
+
+    public function testRemoveTaskRelation()
+    {
+    // Create a User instance
+    $user = new User();
+
+    // Create a Task instance
+    $task = new Task();
+
+    // Add the Task to the User's TaskRelation
+    $user->addTaskRelation($task);
+
+    // Call the removeTaskRelation method
+    $user->removeTaskRelation($task);
+
+    // Assert that the Task has been removed from the User's TaskRelation
+    $taskRelation = $user->getTaskRelation();
+    $this->assertFalse($taskRelation->contains($task));
+    }
+
 }
