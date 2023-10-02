360f16a064f862a7f0e11b96e7d2b73a3add701e
ZAYNA <zk10-91@hotmail.com>
2023-10-02 15:04:44 +0200

    modify for coverage

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,24 +21,6 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    public function save(User $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(User $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */
