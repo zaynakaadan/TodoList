80f96577684d635f9e98485224fba8841723031a
ZAYNA <zk10-91@hotmail.com>
2023-10-02 13:47:01 +0200

    add test login with invalid data

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -11,16 +11,40 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SecurityControllerTest extends WebTestCase
 {
-    public function testLoginAction(): void
+    public function testHomepageLoginWithAdmin(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form([
+            '_username' => 'Administrateur',
+            '_password' => 'password'
+        ]);
+        $client->submit($form);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains("h1", "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
     }
+
+    public function testLoginWithInvalidData()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form([
+            '_username' => 'xxxx@xxxx.com',
+            '_password' => 'incorrectpassword'
+        ]);
+        $client->submit($form);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-danger');
+    }
+
     public function testLogoutCheck(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/logout');
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
     }
+   
 }
